4648b615be1280751212d0730c50954ddbcf9065
Andreia Cifoni <andreiacifoni@gmail.com>
2022-09-17 23:57:12 -0400

    Testing code logic>>  TestCode can find index of tileLevel arrays

--- src/components/TestCode.js
+++ src/components/TestCode.js
@@ -20,10 +20,46 @@ const TestCode = () => {
     id: 3,
   };
 
+  const tile4 = {
+    title: "orange",
+    url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit9_wqgvcg.jpg",
+    id: 4,
+  };
+
+  const tile5 = {
+    title: "kiwi",
+    url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843922/fruits/fruit3_stcebg.jpg",
+    id: 5,
+  };
+
+  const tile6 = {
+    title: "strawberry",
+    url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit7_sljc7e.jpg",
+    id: 6,
+  };
+
+  const tile7 = {
+    title: "orange",
+    url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit9_wqgvcg.jpg",
+    id: 7,
+  };
+
+  const tile8 = {
+    title: "kiwi",
+    url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843922/fruits/fruit3_stcebg.jpg",
+    id: 8,
+  };
+
+  const tile9 = {
+    title: "strawberry",
+    url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit7_sljc7e.jpg",
+    id: 9,
+  };
+
   const tileLevel1 = [
     [tile1, tile2, tile3],
-    [tile2, tile3, tile1],
-    [tile3, tile1, tile2],
+    [tile5, tile6, tile4],
+    [tile9, tile7, tile8],
   ];
 
   const tileLevel2 = [
@@ -32,22 +68,33 @@ const TestCode = () => {
     [tile2, tile3, tile1],
   ];
 
+  const onLevel1Click = (index, x) => {
+    const y = tileLevel1.indexOf(x);
+    console.log(y);
+    console.log(index);
+  };
+
   return (
     <div>
-      <div>
-        {tileLevel1.map((tiles) => {
+      <div className="level1">
+        {tileLevel1.map((tiles, x) => {
           return (
             <div>
-              {tiles.map((tile) => {
+              {tiles.map((tile, index, x) => {
                 return (
-                  <img className="tile-img" src={tile.url} alt={tile.title} />
+                  <img
+                    onClick={() => onLevel1Click(index, x)}
+                    className="tile-img"
+                    src={tile.url}
+                    alt={tile.title}
+                  />
                 );
               })}
             </div>
           );
         })}
       </div>
-      <div>
+      <div className="level2">
         {tileLevel2.map((tiles) => {
           return (
             <div>
