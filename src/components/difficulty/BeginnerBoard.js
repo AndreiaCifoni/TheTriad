44c04c7c5b9a1c2d403b2bb11f3160135f245a13
Andreia Cifoni <andreiacifoni@gmail.com>
2022-09-25 10:14:52 -0400

    refactored

--- src/components/difficulty/BeginnerBoard.js
+++ src/components/difficulty/BeginnerBoard.js
@@ -5,6 +5,7 @@ import {
   tileLevel3,
   beginnerLevelsLayout,
 } from "../../util/tiles/beginnerTiles";
+import { removeTile } from "../../util/tileLevelFunctions";
 import "../../style.css";
 
 const BeginnerBoard = () => {
@@ -12,21 +13,36 @@ const BeginnerBoard = () => {
     const z = index1;
     const x = index2;
     const y = index3;
-    ////tileUpperLevel is returning undefined when > than (length -1)... I tried a if condition:
-    // const tileUpperLevel = () => {
-    //   if(beginnerLevelsLayout[z + 1] > beginnerLevelsLayout.length-1){
-    //     return null
+    const tileLevel = beginnerLevelsLayout[z];
+
+    ////tileUpperLevel is returning "undefined" instead of "null" when > than (length -1)... I tried a if condition:
+    // const tileUpperLevel = (z, beginnerLevelsLayout) => {
+    //   if (beginnerLevelsLayout[z + 1] > beginnerLevelsLayout.length - 1) {
+    //     return null;
     //   }
-    //   return beginnerLevelsLayout[z + 1]
-    // }
+    //   return beginnerLevelsLayout[z + 1];
+    // };
+    // console.log(tileUpperLevel(z, beginnerLevelsLayout));
+
     const tileUpperLevel = beginnerLevelsLayout[z + 1];
 
-    ////if I try to introduce this functions that find the corners, it get "undefined"...
-    ////while if I do a console.log(tileUpperLevel[x][y]) I got the result
-    // const getEvenLevelTileBottomRight = (x, y, tileUpperLevel) => {
+    ////the function "getEvenLevelTileBottomRight" is here just to test. It will be refactored to fit EVEN and ODD levels
+    // const getEvenLevelTileBottomRight = (
+    //   x,
+    //   y,
+    //   tileUpperLevel,
+    //   beginnerLevelsLayout
+    // ) => {
+    //   if (tileUpperLevel > beginnerLevelsLayout.length - 1) {
+    //     return null;
+    //   }
     //   return tileUpperLevel[x][y];
     // };
-    // console.log(getEvenLevelTileBottomRight());
+    // console.log(
+    //   getEvenLevelTileBottomRight(x, y, tileUpperLevel, beginnerLevelsLayout)
+    // );
+
+    removeTile(x, y, tileLevel);
   };
 
   return (
