c7b89623d740749bd5e36db47d6f05c60383d110
Andreia Cifoni <andreiacifoni@gmail.com>
2022-10-04 22:14:31 -0400

    adjusting icon in GameNav

--- src/components/game/GamePageNavBar.js
+++ src/components/game/GamePageNavBar.js
@@ -1,35 +1,40 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { gamePageNavBar } from "../../util/gamePageNavBar";
-import { MenuIcon, XIcon } from "@heroicons/react/solid";
+import { CloudIcon } from "@heroicons/react/24/solid";
 
 const GamePageNavBar = () => {
-  // const [gameNavClicked, setGameNavClicked] = useState(false);
+  const [gameNavClicked, setGameNavClicked] = useState(false);
 
-  // const onClickGameNav = () => {
-  //   setGameNavClicked(!gameNavClicked);
-  // };
+  const onClickGameNav = () => {
+    setGameNavClicked(!gameNavClicked);
+  };
 
-  // const onGameNavIcon = () => {
-  //   setGameNavClicked(!gameNavClicked);
-  // };
+  const onGameNavIcon = () => {
+    setGameNavClicked(!gameNavClicked);
+  };
 
   return (
     <nav className="gamePageNav-container">
       <h1 className="gamePageNav-title">Match 3</h1>
-      {/* <i onClick={onGameNavIcon}>
-        {gameNavClicked ? (
-          <XIcon className="nav-icon-icon" />
-        ) : (
-          <MenuIcon className="nav-icon-icon" />
-        )}
-      </i> */}
+      <div>
+        <CloudIcon className="nav-icon-icon" />
+      </div>
+      {/* <div className="nav-icon">
+        <i onClick={onGameNavIcon}>
+          {gameNavClicked ? (
+            <XIcon className="nav-icon-icon" />
+          ) : (
+            <MenuIcon className="nav-icon-icon" />
+          )}
+        </i>
+      </div> */}
       <ul className="gamePageNav-list">
-        {gamePageNavBar.map((nav) => {
+        {gamePageNavBar.map((nav, index) => {
           return (
-            <li>
+            <li key={index}>
               <Link
-                //onClick={onClickGameNav}
+                onClick={onClickGameNav}
                 className="gamePageNav-item"
                 to={nav.path}
               >
