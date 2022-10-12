dd5c76e8db9581240c817bce3af62c228419c00c
Andreia Cifoni <andreiacifoni@gmail.com>
2022-10-12 14:58:28 -0400

    tiles for new boards

--- src/util/tiles/tilesLayout/tileLayout1.js
+++ src/util/tiles/tilesLayout/tileLayout1.js
@@ -1,3 +1,237 @@
+const tile1 = {
+  title: "orange",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit9_wqgvcg.jpg",
+  id: 1,
+};
+
+const tile2 = {
+  title: "orange",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit9_wqgvcg.jpg",
+  id: 2,
+};
+
+const tile3 = {
+  title: "orange",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit9_wqgvcg.jpg",
+  id: 3,
+};
+
+const tile4 = {
+  title: "cherry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit4_xlmd3h.jpg",
+  id: 4,
+};
+
+const tile5 = {
+  title: "cherry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit4_xlmd3h.jpg",
+  id: 5,
+};
+
+const tile6 = {
+  title: "cherry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit4_xlmd3h.jpg",
+  id: 6,
+};
+
+const tile7 = {
+  title: "kiwi",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843922/fruits/fruit3_stcebg.jpg",
+  id: 7,
+};
+
+const tile8 = {
+  title: "kiwi",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843922/fruits/fruit3_stcebg.jpg",
+  id: 8,
+};
+
+const tile9 = {
+  title: "kiwi",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843922/fruits/fruit3_stcebg.jpg",
+  id: 9,
+};
+
+const tile10 = {
+  title: "strawberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit7_sljc7e.jpg",
+  id: 10,
+};
+
+const tile11 = {
+  title: "strawberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit7_sljc7e.jpg",
+  id: 11,
+};
+
+const tile12 = {
+  title: "strawberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit7_sljc7e.jpg",
+  id: 12,
+};
+
+const tile13 = {
+  title: "pineapple",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit6_nwnuzd.jpg",
+  id: 13,
+};
+
+const tile14 = {
+  title: "pineapple",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit6_nwnuzd.jpg",
+  id: 14,
+};
+
+const tile15 = {
+  title: "pineapple",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit6_nwnuzd.jpg",
+  id: 15,
+};
+
+const tile16 = {
+  title: "watermelon",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit5_gskfwh.jpg",
+  id: 16,
+};
+
+const tile17 = {
+  title: "watermelon",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit5_gskfwh.jpg",
+  id: 17,
+};
+
+const tile18 = {
+  title: "watermelon",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit5_gskfwh.jpg",
+  id: 18,
+};
+
+const tile19 = {
+  title: "apple",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit1_xb2mlb.jpg",
+  id: 19,
+};
+
+const tile20 = {
+  title: "apple",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit1_xb2mlb.jpg",
+  id: 20,
+};
+
+const tile21 = {
+  title: "apple",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit1_xb2mlb.jpg",
+  id: 21,
+};
+
+const tile22 = {
+  title: "raspberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit11_rjdaww.jpg",
+  id: 22,
+};
+
+const tile23 = {
+  title: "raspberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit11_rjdaww.jpg",
+  id: 23,
+};
+
+const tile24 = {
+  title: "raspberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit11_rjdaww.jpg",
+  id: 24,
+};
+
+const tile25 = {
+  title: "lemon",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit2_tbklqu.jpg",
+  id: 25,
+};
+
+const tile26 = {
+  title: "lemon",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit2_tbklqu.jpg",
+  id: 26,
+};
+
+const tile27 = {
+  title: "lemon",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit2_tbklqu.jpg",
+  id: 27,
+};
+
+const tile28 = {
+  title: "pear",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit8_qw7h5e.jpg",
+  id: 28,
+};
+
+const tile29 = {
+  title: "pear",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit8_qw7h5e.jpg",
+  id: 29,
+};
+
+const tile30 = {
+  title: "pear",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit8_qw7h5e.jpg",
+  id: 30,
+};
+
+const tile31 = {
+  title: "peach",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit10_qwjjwp.jpg",
+  id: 31,
+};
+
+const tile32 = {
+  title: "peach",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit10_qwjjwp.jpg",
+  id: 32,
+};
+
+const tile33 = {
+  title: "peach",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit10_qwjjwp.jpg",
+  id: 33,
+};
+
+const tile34 = {
+  title: "banana",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit12_gsax4a.jpg",
+  id: 34,
+};
+
+const tile35 = {
+  title: "banana",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit12_gsax4a.jpg",
+  id: 35,
+};
+
+const tile36 = {
+  title: "banana",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit12_gsax4a.jpg",
+  id: 36,
+};
+
+const tile37 = {
+  title: "kiwi",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843922/fruits/fruit3_stcebg.jpg",
+  id: 37,
+};
+
+const tile38 = {
+  title: "kiwi",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843922/fruits/fruit3_stcebg.jpg",
+  id: 38,
+};
+
+const tile39 = {
+  title: "kiwi",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843922/fruits/fruit3_stcebg.jpg",
+  id: 39,
+};
+
 const tileLeve1 = [
   [tile, tile, tile, tile, tile],
   [tile, tile, tile, tile, tile],
@@ -12,8 +246,8 @@ const tileLevel2 = [
 ];
 
 const tileLevel3 = [
-  [tile, tile, tile],
-  [tile, tile, tile],
+  [tile, tile, tile, tile],
+  [tile, tile, tile, null],
 ];
 
 const boardLayout = [tileLevel1, tileLevel2, tileLevel3];
--- src/util/tiles/tilesLayout/tileLayout3.js
+++ src/util/tiles/tilesLayout/tileLayout3.js
@@ -1,3 +1,360 @@
+const tile1 = {
+  title: "orange",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit9_wqgvcg.jpg",
+  id: 1,
+};
+
+const tile2 = {
+  title: "orange",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit9_wqgvcg.jpg",
+  id: 2,
+};
+
+const tile3 = {
+  title: "orange",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit9_wqgvcg.jpg",
+  id: 3,
+};
+
+const tile4 = {
+  title: "cherry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit4_xlmd3h.jpg",
+  id: 4,
+};
+
+const tile5 = {
+  title: "cherry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit4_xlmd3h.jpg",
+  id: 5,
+};
+
+const tile6 = {
+  title: "cherry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit4_xlmd3h.jpg",
+  id: 6,
+};
+
+const tile7 = {
+  title: "kiwi",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843922/fruits/fruit3_stcebg.jpg",
+  id: 7,
+};
+
+const tile8 = {
+  title: "kiwi",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843922/fruits/fruit3_stcebg.jpg",
+  id: 8,
+};
+
+const tile9 = {
+  title: "kiwi",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843922/fruits/fruit3_stcebg.jpg",
+  id: 9,
+};
+
+const tile10 = {
+  title: "strawberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit7_sljc7e.jpg",
+  id: 10,
+};
+
+const tile11 = {
+  title: "strawberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit7_sljc7e.jpg",
+  id: 11,
+};
+
+const tile12 = {
+  title: "strawberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit7_sljc7e.jpg",
+  id: 12,
+};
+
+const tile13 = {
+  title: "pineapple",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit6_nwnuzd.jpg",
+  id: 13,
+};
+
+const tile14 = {
+  title: "pineapple",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit6_nwnuzd.jpg",
+  id: 14,
+};
+
+const tile15 = {
+  title: "pineapple",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit6_nwnuzd.jpg",
+  id: 15,
+};
+
+const tile16 = {
+  title: "watermelon",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit5_gskfwh.jpg",
+  id: 16,
+};
+
+const tile17 = {
+  title: "watermelon",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit5_gskfwh.jpg",
+  id: 17,
+};
+
+const tile18 = {
+  title: "watermelon",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit5_gskfwh.jpg",
+  id: 18,
+};
+
+const tile19 = {
+  title: "apple",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit1_xb2mlb.jpg",
+  id: 19,
+};
+
+const tile20 = {
+  title: "apple",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit1_xb2mlb.jpg",
+  id: 20,
+};
+
+const tile21 = {
+  title: "apple",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit1_xb2mlb.jpg",
+  id: 21,
+};
+
+const tile22 = {
+  title: "raspberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit11_rjdaww.jpg",
+  id: 22,
+};
+
+const tile23 = {
+  title: "raspberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit11_rjdaww.jpg",
+  id: 23,
+};
+
+const tile24 = {
+  title: "raspberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit11_rjdaww.jpg",
+  id: 24,
+};
+
+const tile25 = {
+  title: "lemon",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit2_tbklqu.jpg",
+  id: 25,
+};
+
+const tile26 = {
+  title: "lemon",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit2_tbklqu.jpg",
+  id: 26,
+};
+
+const tile27 = {
+  title: "lemon",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit2_tbklqu.jpg",
+  id: 27,
+};
+
+const tile28 = {
+  title: "pear",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit8_qw7h5e.jpg",
+  id: 28,
+};
+
+const tile29 = {
+  title: "pear",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit8_qw7h5e.jpg",
+  id: 29,
+};
+
+const tile30 = {
+  title: "pear",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit8_qw7h5e.jpg",
+  id: 30,
+};
+
+const tile31 = {
+  title: "peach",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit10_qwjjwp.jpg",
+  id: 31,
+};
+
+const tile32 = {
+  title: "peach",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit10_qwjjwp.jpg",
+  id: 32,
+};
+
+const tile33 = {
+  title: "peach",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit10_qwjjwp.jpg",
+  id: 33,
+};
+
+const tile34 = {
+  title: "banana",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit12_gsax4a.jpg",
+  id: 34,
+};
+
+const tile35 = {
+  title: "banana",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit12_gsax4a.jpg",
+  id: 35,
+};
+
+const tile36 = {
+  title: "banana",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit12_gsax4a.jpg",
+  id: 36,
+};
+
+const tile37 = {
+  title: "kiwi",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843922/fruits/fruit3_stcebg.jpg",
+  id: 37,
+};
+
+const tile38 = {
+  title: "kiwi",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843922/fruits/fruit3_stcebg.jpg",
+  id: 38,
+};
+
+const tile39 = {
+  title: "kiwi",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843922/fruits/fruit3_stcebg.jpg",
+  id: 39,
+};
+
+const tile40 = {
+  title: "orange",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit9_wqgvcg.jpg",
+  id: 40,
+};
+
+const tile41 = {
+  title: "orange",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit9_wqgvcg.jpg",
+  id: 41,
+};
+
+const tile42 = {
+  title: "orange",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit9_wqgvcg.jpg",
+  id: 42,
+};
+const tile43 = {
+  title: "cherry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit4_xlmd3h.jpg",
+  id: 43,
+};
+
+const tile44 = {
+  title: "cherry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit4_xlmd3h.jpg",
+  id: 44,
+};
+
+const tile45 = {
+  title: "cherry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit4_xlmd3h.jpg",
+  id: 45,
+};
+
+const tile46 = {
+  title: "apple",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit1_xb2mlb.jpg",
+  id: 46,
+};
+
+const tile47 = {
+  title: "apple",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit1_xb2mlb.jpg",
+  id: 47,
+};
+
+const tile48 = {
+  title: "apple",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit1_xb2mlb.jpg",
+  id: 48,
+};
+const tile49 = {
+  title: "raspberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit11_rjdaww.jpg",
+  id: 49,
+};
+
+const tile50 = {
+  title: "raspberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit11_rjdaww.jpg",
+  id: 50,
+};
+
+const tile51 = {
+  title: "raspberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit11_rjdaww.jpg",
+  id: 51,
+};
+const tile52 = {
+  title: "lemon",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit2_tbklqu.jpg",
+  id: 52,
+};
+
+const tile53 = {
+  title: "lemon",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit2_tbklqu.jpg",
+  id: 53,
+};
+
+const tile54 = {
+  title: "lemon",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843921/fruits/fruit2_tbklqu.jpg",
+  id: 54,
+};
+
+const tile55 = {
+  title: "peach",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit10_qwjjwp.jpg",
+  id: 55,
+};
+
+const tile56 = {
+  title: "peach",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit10_qwjjwp.jpg",
+  id: 56,
+};
+
+const tile57 = {
+  title: "peach",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1663431780/fruits/fruit10_qwjjwp.jpg",
+  id: 57,
+};
+
+const tile58 = {
+  title: "strawberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit7_sljc7e.jpg",
+  id: 58,
+};
+
+const tile59 = {
+  title: "strawberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit7_sljc7e.jpg",
+  id: 59,
+};
+
+const tile60 = {
+  title: "strawberry",
+  url: "https://res.cloudinary.com/deiacifoni/image/upload/v1662843920/fruits/fruit7_sljc7e.jpg",
+  id: 60,
+};
+
 const tileLeve1 = [
   [null, null, tile, tile, tile, tile, tile],
   [tile, null, null, tile, tile, tile, tile],
