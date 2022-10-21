9b554ba42463deae574b261cdc3464d360f3f8c7
Andreia Cifoni <andreiacifoni@gmail.com>
2022-10-21 18:27:31 -0400

    routes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./components/App";
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <HashRouter>
+  <HashRouter basename={"/"}>
     <App />
   </HashRouter>
 );
