dd1c20f0ef6bdd6b5fc78990bd0a6108f292c097
Andreia Cifoni <andreiacifoni@gmail.com>
2022-10-21 18:06:45 -0400

    fix BrowserRouter

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./components/App";
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 );
