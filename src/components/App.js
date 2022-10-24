2ca0b44478296d785ecd66bfb8f326db5df4dd80
Andreia Cifoni <andreiacifoni@gmail.com>
2022-10-23 22:15:43 -0400

    tryed to randomize boards

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,37 @@ import TestCode from "./TestCode";
 import GameApp from "./game/GameApp";
 import WinLoseCard from "./game/WinLoseCard";
 import { boardLayout } from "../util/tiles/beginnerTiles";
+import { boardLayoutBeginner1 } from "../util/tiles/tilesLayout/tileLayoutBeginner1";
+import { boardLayoutBeginner2 } from "../util/tiles/tilesLayout/tileLayoutBeginner2";
 import { boardLayoutBeginner3 } from "../util/tiles/tilesLayout/tileLayoutBeginner3";
+import { boardLayoutIntermediate1 } from "../util/tiles/tilesLayout/tileLayoutIntermediate1";
+import { boardLayoutIntermediate2 } from "../util/tiles/tilesLayout/tileLayoutIntermediate2";
 import { boardLayoutIntermediate3 } from "../util/tiles/tilesLayout/tileLayoutIntermediate3";
+import { boardLayoutExpert1 } from "../util/tiles/tilesLayout/tileLayoutExpert1";
+import { boardLayoutExpert2 } from "../util/tiles/tilesLayout/tileLayoutExpert2";
 import { boardLayoutExpert3 } from "../util/tiles/tilesLayout/tileLayoutExpert3";
 
 const App = () => {
+  const beginnerBoards = [
+    boardLayoutBeginner1,
+    boardLayoutBeginner2,
+    boardLayoutBeginner3,
+  ];
+
+  const intermediateBoards = [
+    boardLayoutIntermediate1,
+    boardLayoutIntermediate2,
+    boardLayoutIntermediate3,
+  ];
+
+  const expertBoards = [
+    boardLayoutExpert1,
+    boardLayoutExpert2,
+    boardLayoutExpert3,
+  ];
+
+  const newBoard = Math.floor(Math.random() * 3);
+
   return (
     <div className="app-container">
       <Routes>
@@ -22,16 +48,19 @@ const App = () => {
         />
         <Route
           path={"/game-beginner"}
-          element={<GameApp boardLayout={boardLayoutBeginner3} />}
+          element={<GameApp boardLayout={beginnerBoards[newBoard]} />}
         />
+
         <Route
           path={"/game-intermediate"}
-          element={<GameApp boardLayout={boardLayoutIntermediate3} />}
+          element={<GameApp boardLayout={intermediateBoards[newBoard]} />}
         />
+
         <Route
           path={"/game-expert"}
-          element={<GameApp boardLayout={boardLayoutExpert3} />}
+          element={<GameApp boardLayout={expertBoards[newBoard]} />}
         />
+
         <Route path={"/winlose"} element={<WinLoseCard />} />
         <Route exact path={"/"} element={<Home />} />
       </Routes>
