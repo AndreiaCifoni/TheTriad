aa8d4291df18011b6763a190bcaaf1fd347cf4d5
Andreia Cifoni <andreiacifoni@gmail.com>
2022-10-25 15:12:48 -0400

    Home rules icon fixed dimension and spacing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
 import Rules from "./Rules";
+import { XIcon } from "@heroicons/react/solid";
 
 const Home = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const onToggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <div className="home-container">
-      <button className="home-rules-btn">Rules</button>
+      <button
+        className={toggle ? "home-rules-icon" : "home-rules-btn"}
+        onClick={onToggle}
+      >
+        {toggle ? <XIcon className="home-icon" /> : "Rules"}
+      </button>
       <div className="home-rules">
         <Rules />
       </div>
